574064e1a48710d2068fb0f6197e3c8d9f0ed829
MaksiConGit <122688647+MaksiConGit@users.noreply.github.com>
2024-09-10 21:24:22 -0300

    feat: se añade mvc de subjects

--- code/gestion-educativa/routes/web.php
+++ code/gestion-educativa/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SubjectController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('subjects', SubjectController::class);
