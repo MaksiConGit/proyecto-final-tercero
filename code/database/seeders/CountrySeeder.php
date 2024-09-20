7b2190114606f7ebf820e6d59ff415ac0375691a
Lucas Del Labo <lucas.dellabo@gmail.com>
2024-09-20 17:47:21 -0300

    feat: Country Model, Migration, Seed
    Se añaden el modelo, migración y seed de Country

---
+++ code/database/seeders/CountrySeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Country;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CountrySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Country::create([
+            'name' => 'Argentina',
+        ]);
+    }
+}
