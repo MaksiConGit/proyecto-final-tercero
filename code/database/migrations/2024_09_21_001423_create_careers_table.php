7d67719c1931637669a75c25f8cc442532241c63
Lucas Del Labo <lucas.dellabo@gmail.com>
2024-09-20 21:21:30 -0300

    feat: Career Model, Migration
    Se añaden el modelo y migración de Career, además se agregan modelos y migraciones default de Institution para las relaciones.

---
+++ code/database/migrations/2024_09_21_001423_create_careers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('careers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('institution_id')->constrained()
+                                        ->onUpdate('cascade')
+                                        ->onDelete('restrict');
+            $table->boolean('is_deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('careers');
+    }
+};
