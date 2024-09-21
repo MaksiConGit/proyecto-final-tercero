996acfa1513a978f0505eab34e346798fecf3751
Lucas Del Labo <lucas.dellabo@gmail.com>
2024-09-20 21:40:00 -0300

    feat: Course Model, Migration
    Se añaden el modelo y migración de Course, además se agregan modelos y migraciones default de Career para las relaciones.

---
+++ code/app/Models/Career.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Career extends Model
+{
+    use HasFactory;
+}
