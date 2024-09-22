354b8c79760ef163f95f1a98e8b99e6e9a1b3e20
MaksiConGit <122688647+MaksiConGit@users.noreply.github.com>
2024-09-22 10:00:04 -0300

    feat: Model, Migration Student

---
+++ code/app/Http/Requests/StoreStudentRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreStudentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' =>  'required|string|min:2|max:255',
+            'lastname' => 'required|string|min:2|max:255',
+            'dni' => 'required|string|max:255',
+            'phone' => 'required|string|max:14',
+            'birthdate' => 'required|date|before:today',
+            'city_id' => 'required|exists:cities,id',
+            'user_id' => 'required|exists:users,id',
+            'is_deleted' => 'boolean'
+        ];
+    }
+}
