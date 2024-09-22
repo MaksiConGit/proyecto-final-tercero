e4536db43d20a052cf24cf53e25504db4d8a567b
MaksiConGit <122688647+MaksiConGit@users.noreply.github.com>
2024-09-22 10:03:04 -0300

    feat: Model, Migration Teacher

---
+++ code/database/migrations/2024_09_22_130105_create_teachers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('lastname');
+            $table->string('dni')->unique();
+            $table->string('phone');
+            $table->date('birthdate');
+            $table->foreignId('city_id')->constrained()
+                                        ->onDelete('restrict')
+                                        ->onUpdate('cascade');
+            $table->foreignId('user_id')->constrained()
+                                        ->onDelete('restrict')
+                                        ->onUpdate('cascade');
+            $table->boolean('is_deleted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teachers');
+    }
+};
