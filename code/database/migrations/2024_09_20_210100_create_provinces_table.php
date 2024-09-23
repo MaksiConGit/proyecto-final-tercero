4ac24a743fc0ae24873bc9ffb203f55f5d9d79e7
MaksiConGit <122688647+MaksiConGit@users.noreply.github.com>
2024-09-23 17:00:46 -0300

    fix: Request, Asignación Masiva, Migración
    Añade request, quita la asignación masiva, y se añade unique() en name en la migración.

--- code/database/migrations/2024_09_20_210100_create_provinces_table.php
+++ code/database/migrations/2024_09_20_210100_create_provinces_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('provinces', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->foreignId('country_id')->constrained()
                                         ->onUpdate('cascade')
                                         ->onDelete('restrict');
