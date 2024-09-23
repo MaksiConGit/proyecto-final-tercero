ad0dfb891df26d516b0c692e1db3dc6c2f8a04fd
MaksiConGit <122688647+MaksiConGit@users.noreply.github.com>
2024-09-23 09:39:27 -0300

    feat: Model, Migration Grade

---
+++ code/database/migrations/2024_09_23_123237_create_grades_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('grades', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->constrained()->onDelete('restrict')->onUpdate('cascade');
+            $table->foreignId('exam_id')->constrained()->onDelete('restrict')->onUpdate('cascade');
+            $table->integer('grade');
+            $table->boolean('is_deleted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('grades');
+    }
+};
