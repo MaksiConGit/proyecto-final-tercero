ebddb98c0a5aa5a7775539eccb00035666f7f87c
MaksiConGit <122688647+MaksiConGit@users.noreply.github.com>
2024-09-26 19:23:30 -0300

    fix: se agrega is_deleted y sus validaciones

--- code/app/Http/Requests/StorePermissionRequest.php
+++ code/app/Http/Requests/StorePermissionRequest.php
@@ -22,7 +22,8 @@ class StorePermissionRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|max:255'
+            'name' => 'required|string|max:255',
+            'is_deleted' => 'boolean'
         ];
     }
 }
