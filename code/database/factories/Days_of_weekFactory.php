99fcea6ea8e8d91f9e3e90f043a449eb8b395263
MaksiConGit <122688647+MaksiConGit@users.noreply.github.com>
2024-10-04 20:02:33 -0300

    partial feat: Seeder Days_of_week

---
+++ code/database/factories/Days_of_weekFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Days_of_week>
+ */
+class Days_of_weekFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
