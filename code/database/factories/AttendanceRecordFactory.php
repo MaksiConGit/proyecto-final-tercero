ee008af7ea6f8fccdc1bf10bb5d752f37a04332c
LucasDLabo <lucas.dellabo@gmail.com>
2024-10-05 20:48:29 -0300

    feat: Factory AttendanceRecords

---
+++ code/database/factories/AttendanceRecordFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Student;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Testing\Fakes\Fake;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AttendanceRecord>
+ */
+class AttendanceRecordFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'student_id' => Student::inRandomOrder()->first()->id,
+            'has_attended'=> fake()->boolean(),
+            'date'=>fake()->date()
+        ];
+    }
+}
