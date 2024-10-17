50859402818bfd2ea1be3a411dbfe93bb1762432
Lucas Del Labo <lucas.dellabo@gmail.com>
2024-10-17 12:08:13 -0300

    fix: Tabla "subjects", campo "name"->unique

--- code/database/migrations/2024_10_01_000015_create_subjects_table.php
+++ code/database/migrations/2024_10_01_000015_create_subjects_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('subjects', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->boolean('is_deleted')->default(false);
             $table->timestamps();
         });
