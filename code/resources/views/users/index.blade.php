fee56f81e38e7643fe6a393b3665b7984f9a5899
MaksiConGit <122688647+MaksiConGit@users.noreply.github.com>
2024-10-17 10:30:47 -0300

    feat: CRUD Users

---
+++ code/resources/views/users/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lista de Usuarios</h1>
+    <a href="{{route('users.create')}}">create</a>
+    <ul>
+        @foreach ($users as $user)
+            <li><a href="{{route('users.show', [$user])}}"> {{$user->name}} @if ($user->is_deleted)(borrado) @endif </a></li>
+        @endforeach
+    </ul>
+</body>
+</html>
--- code/routes/web.php
+++ code/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,5 +17,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('users', UserController::class)->names('users');
 
 require __DIR__.'/auth.php';
