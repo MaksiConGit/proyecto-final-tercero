21f85d5d369bc5c36ba3c53e9b7da14c3e5686cc
Lucas Del Labo <lucas.dellabo@gmail.com>
2024-10-17 16:05:48 -0300

    feat: CRUD Course
    Se realizó el CRUD de Course con las correspondientes validaciones y mensajes de error. Se añadió la relación con la tabla "careers" para su correcto funcionamiento. Se cambio el atributo authorize() dentro de la request a "true" para su correcto funcionamiento.
    Se corrigió un pequeño error que faltaba una letra "s" en la Request StoreCourse.

---
+++ code/resources/views/courses/edit.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Formulario de Edicion</h1>
+    @if ($errors->any())
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{$error}}</li>    
+            @endforeach
+        </ul>
+    @endif
+    <form method="POST" action="{{route('courses.update', $course)}}">
+        @csrf
+        @method('PUT')
+
+        <label>
+            course_number:
+            <input type="text" name="course_number" value="{{old('course_number', $course->course_number) }}" required />
+        </label>
+        <br>
+        <label>
+            section:
+            <input type="text" name="section" value="{{old('section', $course->section) }}" required />
+        </label>
+
+        </div>
+        <button type="submit"> update </button>
+    </form>
+</body>
+</html>
---
+++ code/resources/views/courses/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Cursos</h1>
+    <a href="{{route('courses.create')}}">create</a>
+    <ul>
+        @foreach ($courses as $course)
+        <li><a href="{{route('courses.show', [$course])}}"> {{$course->course_number}}° {{$course->section}}</a></li>
+        @endforeach
+    </ul>
+</body>
+</html>
