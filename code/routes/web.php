a4076035978c8827f7550315e71c730d21810a92
Marzopax <lamarzopadelmar@gmail.com>
2024-11-04 09:33:41 -0300

    feat<front>: publicar

--- code/routes/web.php
+++ code/routes/web.php
@@ -13,6 +13,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/publicar', function () {
+    return view('profesorPublicar');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })
