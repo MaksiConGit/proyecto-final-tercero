0f2a9f96cf149d733e2a2687c34c73e373d08cd2
Marzopax <lamarzopadelmar@gmail.com>
2024-11-07 13:22:37 -0300

    Update profesorPublicar.blade.php

--- code/resources/views/profesorPublicar.blade.php
+++ code/resources/views/profesorPublicar.blade.php
@@ -4,22 +4,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Materia: Base de Datos</title>
+    <title>Profesor: Publicar</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
+
 </head>
 
-<body class="bg-gray-200">
+<body class="h-screen flex flex-col">
 
     <header class="bg-purple-900 text-white px-5 py-2 ">
         <div class="m-0 flex items-center justify-between ">
             <h1 class="text-2xl font-bold">Instituto</h1>
             <div class="w-8 h-8">
-                <img src="../images/icono.png" alt="Icono de usuario">
+                <img src="../images/usuario_foto.png" alt="Icono de usuario">
             </div>
         </div>
     </header>
-
+    <!-- Barra de Navegación -->
     <nav class="bg-purple-800 text-white p-4">
         <ol class="flex items-center justify-center space-x-2">
             <li>
@@ -41,11 +42,110 @@
                 </svg>
             </li>
             <li>
-                <a href="../images/materia.png" class="text-white hover:underline font-semibold">Base de Datos</a>
+                <a href="#" class="text-white hover:underline font-semibold">Horarios</a>
             </li>
         </ol>
         </div>
     </nav>
+
+    <!-- Layout Principal -->
+    <div class="flex flex-grow">
+        <!-- Aside Izquierdo -->
+        <aside class="bg-gray-200 w-48 p-2 hidden md:block border-r border-gray-400">
+            <h2 class="text-xl font-bold flex justify-center pb-2 border-b-2 border-gray-400">Carreras</h2>
+            <div id="accordion-flush" data-accordion="collapse" data-active-classes="text-gray-500 bg-gray-300"
+                data-inactive-classes="text-gray-500">
+                <h2 id="accordion-flush-heading-1">
+                    <button type="button"
+                        class="flex items-center justify-between w-full py-5 font-medium text-gray-600 border-b border-gray-200 dark:border-gray-400 gap-3"
+                        data-accordion-target="#accordion-flush-body-1" aria-expanded="false"
+                        aria-controls="accordion-flush-body-1">
+                        <span class="ml-2">Sistemas</span>
+                        <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M9 5 5 1 1 5" />
+                        </svg>
+                    </button>
+                </h2>
+                <div id="accordion-flush-body-1" class="hidden" aria-labelledby="accordion-flush-heading-1">
+                    <div class="border-b border-gray-200">
+                        <div class="">
+                            <a href="materia.html"
+                                class="py-2 border-b hover:bg-gray-300 border-gray-400 text-sm text-gray-500 flex justify-center">1ro</a>
+                            <a href="materia.html"
+                                class="py-2 border-b hover:bg-gray-300 border-gray-400 text-sm text-gray-500 flex justify-center">2do</a>
+                            <a href="materia.html"
+                                class="py-2 border-b hover:bg-gray-300 border-gray-400 text-sm text-gray-500 flex justify-center">3ro</a>
+                        </div>
+                    </div>
+                </div>
+
+                <h2 id="accordion-flush-heading-2">
+                    <button type="button"
+                        class="flex items-center justify-between w-full py-5 font-medium text-gray-600 border-b border-gray-200 dark:border-gray-400 gap-3"
+                        data-accordion-target="#accordion-flush-body-2" aria-expanded="false"
+                        aria-controls="accordion-flush-body-2">
+                        <span class="ml-2">Robotica</span>
+                        <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M9 5 5 1 1 5" />
+                        </svg>
+                    </button>
+                </h2>
+                <div id="accordion-flush-body-2" class="hidden" aria-labelledby="accordion-flush-heading-2">
+                    <div class="border-b border-gray-200">
+                        <div class="">
+                            <a href="materia.html"
+                                class="py-2 border-b hover:bg-gray-300 border-gray-400 text-sm text-gray-500 flex justify-center">1ro</a>
+                            <a href="materia.html"
+                                class="py-2 border-b hover:bg-gray-300 border-gray-400 text-sm text-gray-500 flex justify-center">2do</a>
+                            <a href="materia.html"
+                                class="py-2 border-b hover:bg-gray-300 border-gray-400 text-sm text-gray-500 flex justify-center">3ro</a>
+                        </div>
+                    </div>
+                </div>
+                <h2 id="accordion-flush-heading-3">
+                    <button type="button"
+                        class="flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-600 border-b border-gray-200 dark:border-gray-400 gap-3"
+                        data-accordion-target="#accordion-flush-body-3" aria-expanded="false"
+                        aria-controls="accordion-flush-body-3">
+                        <span class="ml-2">Industrial</span>
+                        <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="2" d="M9 5 5 1 1 5" />
+                        </svg>
+                    </button>
+                </h2>
+                <div id="accordion-flush-body-3" class="hidden" aria-labelledby="accordion-flush-heading-3">
+                    <div class="border-b border-gray-200">
+                        <div class="">
+                            <a href="materia.html"
+                                class="py-2 border-b hover:bg-gray-300 border-gray-400 text-sm text-gray-500 flex justify-center">1ro</a>
+                            <a href="materia.html"
+                                class="py-2 border-b hover:bg-gray-300 border-gray-400 text-sm text-gray-500 flex justify-center">2do</a>
+                            <a href="materia.html"
+                                class="py-2 border-b hover:bg-gray-300 border-gray-400 text-sm text-gray-500 flex justify-center">3ro</a>
+                        </div>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </aside>
+
+        <!-- Contenido Principal Centrado -->
+        <main class="flex-grow flex items-center justify-evenly bg-gray-50 mt-4 mb-4">
+
+        </main>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
+
+</body>
+
+</html>
 
     <div class="flex">
         <div class="flex mt-2">
@@ -428,4 +528,4 @@
 
 </body>
 
-</html>
+</html> --}}
