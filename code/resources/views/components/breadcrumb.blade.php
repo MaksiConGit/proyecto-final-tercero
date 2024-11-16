1c9ab70f36ac1e93c3fce10c724204b96584a1f0
Marzopax <lamarzopadelmar@gmail.com>
2024-11-16 18:48:10 -0300

    feat<front>: horarios + tablas
    tablas editables

--- code/resources/views/components/breadcrumb.blade.php
+++ code/resources/views/components/breadcrumb.blade.php
@@ -1,12 +1,11 @@
 <div class="container d-flex justify-content-center">
-
     <nav aria-label="breadcrumb">
-      <ol class="breadcrumb breadcrumb-style1">
-        <li class="breadcrumb-item">
-          <a href="javascript:void(0);">Home</a>
-        </li>
-        <li class="breadcrumb-item active">Materias</li>
-      </ol>
+        <ol class="breadcrumb breadcrumb-style1">
+            <li class="breadcrumb-item">
+                <a href="javascript:void(0);">Home</a>
+            </li>
+            <li class="breadcrumb-item active">Materias</li>
+        </ol>
     </nav>
-    
+
 </div>
