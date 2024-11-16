73c910f084c8635a172254745ff7ca9c3638d728
MaksiConGit <122688647+MaksiConGit@users.noreply.github.com>
2024-11-16 16:48:49 -0300

    feat<back-front>: se añaden bocetos de diseño de asistencia y materias

--- code/resources/views/bocetos/alumnos.blade.php
+++
@@ -1,9 +0,0 @@
-<x-template-layout>
-    <div class="container">
-        
-        <x-list-group-numered>
-            <x-slot name="titulo">Alumnos</x-slot> 
-            <x-slot name="nombre">tefi</x-slot>
-        </x-list-group-numered>
-    </div>
-</x-template-layout>
