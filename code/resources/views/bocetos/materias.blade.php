f268536aba974cc9100f866a6bba8a38489d3d9c
Marzopax <lamarzopadelmar@gmail.com>
2024-11-16 18:48:10 -0300

    feat<front>: horarios + tablas
    tablas editables

--- code/resources/views/bocetos/materias.blade.php
+++ code/resources/views/bocetos/materias.blade.php
@@ -1,103 +1,116 @@
 <x-template-layout>
-        <div class="container">
-        
-          <div class="row">
+    {{-- aplicar la el componente card --}}
+    <div class="container">
+        <div class="row">
             <div class="col-md-6 col-lg-4 mb-3 d-flex">
-              <div class="card h-100">
-                <img class="card-img-top" src="../../template_files/assets/img/reyo/programacion-2-e1551291144973.jpg" alt="Card image cap" />
-                <div class="card-body">
-                  <h5 class="card-title">Base de datos</h5>
-                  <p class="card-text">Nicolás, Rotili</p>
-                  <p class="card-text">Lun. 18h a 20h</p>
+                <div class="card h-100">
+                    <img class="card-img-top" src="../../template_files/assets/img/reyo/programacion-2-e1551291144973.jpg"
+                        alt="Card image cap" />
+                    <div class="card-body">
+                        <h5 class="card-title">Base de datos</h5>
+                        <p class="card-text">Nicolás, Rotili</p>
+                        <p class="card-text">Lun. 18h a 20h</p>
+                    </div>
                 </div>
-              </div>
             </div>
             <div class="col-md-6 col-lg-4 mb-3 d-flex">
-              <div class="card h-100">
-                <img class="card-img-top" src="../../template_files/assets/img/reyo/educacionfisica.jpg" alt="Card image cap" />
-                <div class="card-body">
-                  <h5 class="card-title">Base de datos</h5>
-                  <p class="card-text">Nicolás, Rotili</p>
-                  <p class="card-text">Lun. 18h a 20h</p>
+                <div class="card h-100">
+                    <img class="card-img-top" src="../../template_files/assets/img/reyo/educacionfisica.jpg"
+                        alt="Card image cap" />
+                    <div class="card-body">
+                        <h5 class="card-title">Base de datos</h5>
+                        <p class="card-text">Nicolás, Rotili</p>
+                        <p class="card-text">Lun. 18h a 20h</p>
+                    </div>
                 </div>
-              </div>
             </div>
             <div class="col-md-6 col-lg-4 mb-3 d-flex">
-              <div class="card h-100">
-                <img class="card-img-top" src="../../template_files/assets/img/reyo/geografia-1-e1571191539294-800x416.jpg" alt="Card image cap" />
-                <div class="card-body">
-                  <h5 class="card-title">Base de datos</h5>
-                  <p class="card-text">Nicolás, Rotili</p>
-                  <p class="card-text">Lun. 18h a 20h</p>
+                <div class="card h-100">
+                    <img class="card-img-top"
+                        src="../../template_files/assets/img/reyo/geografia-1-e1571191539294-800x416.jpg"
+                        alt="Card image cap" />
+                    <div class="card-body">
+                        <h5 class="card-title">Base de datos</h5>
+                        <p class="card-text">Nicolás, Rotili</p>
+                        <p class="card-text">Lun. 18h a 20h</p>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
-
-          <div class="row">
-            <div class="col-md-6 col-lg-4 mb-3 d-flex">
-              <div class="card h-100">
-                <img class="card-img-top" src="../../template_files/assets/img/reyo/programacion-2-e1551291144973.jpg" alt="Card image cap" />
-                <div class="card-body">
-                  <h5 class="card-title">Base de datos</h5>
-                  <p class="card-text">Nicolás, Rotili</p>
-                  <p class="card-text">Lun. 18h a 20h</p>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-6 col-lg-4 mb-3 d-flex">
-              <div class="card h-100">
-                <img class="card-img-top" src="../../template_files/assets/img/reyo/educacionfisica.jpg" alt="Card image cap" />
-                <div class="card-body">
-                  <h5 class="card-title">Base de datos</h5>
-                  <p class="card-text">Nicolás, Rotili</p>
-                  <p class="card-text">Lun. 18h a 20h</p>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-6 col-lg-4 mb-3 d-flex">
-              <div class="card h-100">
-                <img class="card-img-top" src="../../template_files/assets/img/reyo/geografia-1-e1571191539294-800x416.jpg" alt="Card image cap" />
-                <div class="card-body">
-                  <h5 class="card-title">Base de datos</h5>
-                  <p class="card-text">Nicolás, Rotili</p>
-                  <p class="card-text">Lun. 18h a 20h</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-6 col-lg-4 mb-3 d-flex">
-              <div class="card h-100">
-                <img class="card-img-top" src="../../template_files/assets/img/reyo/programacion-2-e1551291144973.jpg" alt="Card image cap" />
-                <div class="card-body">
-                  <h5 class="card-title">Base de datos</h5>
-                  <p class="card-text">Nicolás, Rotili</p>
-                  <p class="card-text">Lun. 18h a 20h</p>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-6 col-lg-4 mb-3 d-flex">
-              <div class="card h-100">
-                <img class="card-img-top" src="../../template_files/assets/img/reyo/educacionfisica.jpg" alt="Card image cap" />
-                <div class="card-body">
-                  <h5 class="card-title">Base de datos</h5>
-                  <p class="card-text">Nicolás, Rotili</p>
-                  <p class="card-text">Lun. 18h a 20h</p>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-6 col-lg-4 mb-3 d-flex">
-              <div class="card h-100">
-                <img class="card-img-top" src="../../template_files/assets/img/reyo/geografia-1-e1571191539294-800x416.jpg" alt="Card image cap" />
-                <div class="card-body">
-                  <h5 class="card-title">Base de datos</h5>
-                  <p class="card-text">Nicolás, Rotili</p>
-                  <p class="card-text">Lun. 18h a 20h</p>
-                </div>
-              </div>
-            </div>
-          </div>
-
         </div>
+
+        <div class="row">
+            <div class="col-md-6 col-lg-4 mb-3 d-flex">
+                <div class="card h-100">
+                    <img class="card-img-top"
+                        src="../../template_files/assets/img/reyo/programacion-2-e1551291144973.jpg"
+                        alt="Card image cap" />
+                    <div class="card-body">
+                        <h5 class="card-title">Base de datos</h5>
+                        <p class="card-text">Nicolás, Rotili</p>
+                        <p class="card-text">Lun. 18h a 20h</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-3 d-flex">
+                <div class="card h-100">
+                    <img class="card-img-top" src="../../template_files/assets/img/reyo/educacionfisica.jpg"
+                        alt="Card image cap" />
+                    <div class="card-body">
+                        <h5 class="card-title">Base de datos</h5>
+                        <p class="card-text">Nicolás, Rotili</p>
+                        <p class="card-text">Lun. 18h a 20h</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-3 d-flex">
+                <div class="card h-100">
+                    <img class="card-img-top"
+                        src="../../template_files/assets/img/reyo/geografia-1-e1571191539294-800x416.jpg"
+                        alt="Card image cap" />
+                    <div class="card-body">
+                        <h5 class="card-title">Base de datos</h5>
+                        <p class="card-text">Nicolás, Rotili</p>
+                        <p class="card-text">Lun. 18h a 20h</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 col-lg-4 mb-3 d-flex">
+                <div class="card h-100">
+                    <img class="card-img-top"
+                        src="../../template_files/assets/img/reyo/programacion-2-e1551291144973.jpg"
+                        alt="Card image cap" />
+                    <div class="card-body">
+                        <h5 class="card-title">Base de datos</h5>
+                        <p class="card-text">Nicolás, Rotili</p>
+                        <p class="card-text">Lun. 18h a 20h</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-3 d-flex">
+                <div class="card h-100">
+                    <img class="card-img-top" src="../../template_files/assets/img/reyo/educacionfisica.jpg"
+                        alt="Card image cap" />
+                    <div class="card-body">
+                        <h5 class="card-title">Base de datos</h5>
+                        <p class="card-text">Nicolás, Rotili</p>
+                        <p class="card-text">Lun. 18h a 20h</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-3 d-flex">
+                <div class="card h-100">
+                    <img class="card-img-top"
+                        src="../../template_files/assets/img/reyo/geografia-1-e1571191539294-800x416.jpg"
+                        alt="Card image cap" />
+                    <div class="card-body">
+                        <h5 class="card-title">Base de datos</h5>
+                        <p class="card-text">Nicolás, Rotili</p>
+                        <p class="card-text">Lun. 18h a 20h</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-template-layout>
