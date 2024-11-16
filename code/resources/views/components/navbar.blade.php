7755ab9199b70a884940c19e7ec9709cb0a5b9eb
Marzopax <lamarzopadelmar@gmail.com>
2024-11-16 18:48:10 -0300

    feat<front>: horarios + tablas
    tablas editables

--- code/resources/views/components/navbar.blade.php
+++ code/resources/views/components/navbar.blade.php
@@ -126,19 +126,52 @@
                 <i class="bx bx-power-off me-2"></i>
                 <span class="align-middle">Salir</span>
                 </a>
+                <ul class="dropdown-menu dropdown-menu-end">
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <div class="d-flex">
+                                <div class="flex-shrink-0 me-3">
+                                    <div class="avatar avatar-online">
+                                        <img src="../template_files/assets/img/avatars/1.png" alt
+                                            class="w-px-40 h-auto rounded-circle" />
+                                    </div>
+                                </div>
+                                <div class="flex-grow-1">
+                                    <span class="fw-semibold d-block">Joako</span>
+                                    <small class="text-muted">Rodriguez</small>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <div class="dropdown-divider"></div>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <i class="bx bx-user me-2"></i>
+                            <span class="align-middle">Mi perfil</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <i class="bx bx-cog me-2"></i>
+                            <span class="align-middle">Opciones</span>
+                        </a>
+                    </li>
 
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}">
-                        Register
-                    </a>
-                @endif
-            @endauth
-        </nav>
-    @endif
-
-        
-        <!--/ User -->
-      </ul>
+                    <li>
+                        <div class="dropdown-divider"></div>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="auth-login-basic.html">
+                            <i class="bx bx-power-off me-2"></i>
+                            <span class="align-middle">Salir</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <!--/ User -->
+        </ul>
     </div>
 
 </nav>
