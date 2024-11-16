617728b3442eae6b31360d9b3cabf7f5828bc36f
Marzopax <lamarzopadelmar@gmail.com>
2024-11-16 18:48:10 -0300

    feat<front>: horarios + tablas
    tablas editables

--- code/resources/views/bocetos/asistencias.blade.php
+++ code/resources/views/bocetos/asistencias.blade.php
@@ -78,12 +78,14 @@
                         <canvas id="doughnutChart4" class="chartjs mb-3" data-height="350"></canvas>
                         <ul class="doughnut-legend d-flex justify-content-around ps-0 mb-2 mt-6 pt-1">
                             <li class="ct-series-0 d-flex flex-column align-items-center">
-                                <span class="badge-dot mb-2" style="background-color: #666ee8; width: 15px; height: 15px; border-radius: 50%;"></span>
+                                <span class="badge-dot mb-2"
+                                    style="background-color: #666ee8; width: 15px; height: 15px; border-radius: 50%;"></span>
                                 <h6 class="mb-0">Asistencia</h6>
                                 <div class="text-muted">68 %</div>
                             </li>
                             <li class="ct-series-1 d-flex flex-column align-items-center">
-                                <span class="badge-dot mb-2" style="background-color: #D3D3D3; width: 15px; height: 15px; border-radius: 50%;"></span>
+                                <span class="badge-dot mb-2"
+                                    style="background-color: #D3D3D3; width: 15px; height: 15px; border-radius: 50%;"></span>
                                 <h6 class="mb-0">Inasistencia</h6>
                                 <div class="text-muted">32 %</div>
                             </li>
@@ -91,7 +93,7 @@
                     </div>
                 </div>
             </div>
-    
+
             <div class="col-md-4 mb-4">
                 <div class="card" style="max-width: 16rem; margin: auto;">
                     <h5 class="card-header">David, Carretilla</h5>
@@ -99,12 +101,14 @@
                         <canvas id="doughnutChart5" class="chartjs mb-3" data-height="350"></canvas>
                         <ul class="doughnut-legend d-flex justify-content-around ps-0 mb-2 mt-6 pt-1">
                             <li class="ct-series-0 d-flex flex-column align-items-center">
-                                <span class="badge-dot mb-2" style="background-color: #666ee8; width: 15px; height: 15px; border-radius: 50%;"></span>
+                                <span class="badge-dot mb-2"
+                                    style="background-color: #666ee8; width: 15px; height: 15px; border-radius: 50%;"></span>
                                 <h6 class="mb-0">Asistencia</h6>
                                 <div class="text-muted">0 %</div>
                             </li>
                             <li class="ct-series-1 d-flex flex-column align-items-center">
-                                <span class="badge-dot mb-2" style="background-color: #D3D3D3; width: 15px; height: 15px; border-radius: 50%;"></span>
+                                <span class="badge-dot mb-2"
+                                    style="background-color: #D3D3D3; width: 15px; height: 15px; border-radius: 50%;"></span>
                                 <h6 class="mb-0">Inasistencia</h6>
                                 <div class="text-muted">100 %</div>
                             </li>
@@ -112,7 +116,7 @@
                     </div>
                 </div>
             </div>
-    
+
             <div class="col-md-4 mb-4">
                 <div class="card" style="max-width: 16rem; margin: auto;">
                     <h5 class="card-header">Cathy, Vipi</h5>
@@ -120,12 +124,14 @@
                         <canvas id="doughnutChart6" class="chartjs mb-3" data-height="350"></canvas>
                         <ul class="doughnut-legend d-flex justify-content-around ps-0 mb-2 mt-6 pt-1">
                             <li class="ct-series-0 d-flex flex-column align-items-center">
-                                <span class="badge-dot mb-2" style="background-color: #666ee8; width: 15px; height: 15px; border-radius: 50%;"></span>
+                                <span class="badge-dot mb-2"
+                                    style="background-color: #666ee8; width: 15px; height: 15px; border-radius: 50%;"></span>
                                 <h6 class="mb-0">Asistencia</h6>
                                 <div class="text-muted">100 %</div>
                             </li>
                             <li class="ct-series-1 d-flex flex-column align-items-center">
-                                <span class="badge-dot mb-2" style="background-color: #D3D3D3; width: 15px; height: 15px; border-radius: 50%;"></span>
+                                <span class="badge-dot mb-2"
+                                    style="background-color: #D3D3D3; width: 15px; height: 15px; border-radius: 50%;"></span>
                                 <h6 class="mb-0">Inasistencia</h6>
                                 <div class="text-muted">0 %</div>
                             </li>
@@ -134,13 +140,13 @@
                 </div>
             </div>
         </div>
-      </div>
+    </div>
 
-      <script>
-        const primaryColor = '#666ee8';  // Color para Asistencia
+    <script>
+        const primaryColor = '#666ee8'; // Color para Asistencia
         const lightGreyColor = '#D3D3D3'; // Gris claro para Inasistencia
         const orangeLightColor = '#FDAC34';
-    
+
         // Script para doughnutChart1
         const ctx1 = document.getElementById('doughnutChart1').getContext('2d');
         const doughnutChart1 = new Chart(ctx1, {
@@ -175,7 +181,7 @@
                 }
             }
         });
-    
+
         // Script para doughnutChart2
         const ctx2 = document.getElementById('doughnutChart2').getContext('2d');
         const doughnutChart2 = new Chart(ctx2, {
@@ -210,7 +216,7 @@
                 }
             }
         });
-    
+
         // Script para doughnutChart3
         const ctx3 = document.getElementById('doughnutChart3').getContext('2d');
         const doughnutChart3 = new Chart(ctx3, {
@@ -278,7 +284,7 @@
                 }
             }
         });
-    
+
         // Script para doughnutChart2
         const ctx5 = document.getElementById('doughnutChart5').getContext('2d');
         const doughnutChart5 = new Chart(ctx5, {
@@ -313,7 +319,7 @@
                 }
             }
         });
-    
+
         // Script para doughnutChart3
         const ctx6 = document.getElementById('doughnutChart6').getContext('2d');
         const doughnutChart6 = new Chart(ctx6, {
