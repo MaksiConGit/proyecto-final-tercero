fd292a30e17b75bd1f1d1d100eedd136c0dd3b8e
Marzopax <lamarzopadelmar@gmail.com>
2024-11-20 11:52:44 -0300

    feat<front>: template materia profesor
    + componente card material y modificacion de card professor

---
+++ code/resources/views/components/card-professor.blade.php
@@ -0,0 +1,18 @@
+<div class="row mb-1">
+    <div class="col-md">
+        <div class="card">
+            <div class="row g-8 ps-2 pt-2 pb-2">
+                <div class="col-md-2">
+                    <img class="card-img card-img-left h-100 w-100 rounded-circle" src="../../template_files/assets/img/elements/12.jpg"
+                        alt="Card image" />
+                </div>
+                <div class="col-md-10">
+                    <div class="card-body ps-0">
+                        <h5 class="card-title">Nicolas Rotilli</h5>
+                        <p class="card-text">nicolas@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
