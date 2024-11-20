60b27d437985645e094ecc0a2865bc924d1c6f37
Marzopax <lamarzopadelmar@gmail.com>
2024-11-20 11:52:44 -0300

    feat<front>: template materia profesor
    + componente card material y modificacion de card professor

--- code/routes/web.php
+++ code/routes/web.php
@@ -13,6 +13,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/profesor', function () {
+    return view('../bocetos/profesorMaterias');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })
