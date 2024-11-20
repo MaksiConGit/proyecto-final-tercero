72dbdc1a975ce7fab90f70b25feea2bcb551a801
Marzopax <lamarzopadelmar@gmail.com>
2024-11-20 19:19:15 -0300

    feat<front>: arreglo de template
    el index esta, la view intermedia que tiene el editar y eliminar no y el editar esta a la mitad

--- code/resources/views/users/edit.blade.php
+++ code/resources/views/users/edit.blade.php
@@ -1,50 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Formulario de Edición de Usuarios</h1>
-    @if ($errors->any())
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{$error}}</li>    
-            @endforeach
-        </ul>
-    @endif
-    <form method="POST" action="{{route('users.update', $user)}}">
-        @csrf
-        @method('PUT')
-        <label>
-            name:
-            <input type="text" name="name" value="{{old('name', $user->name) }}"  required />
-        </label>
-        <br>
-        <label>
-            email:
-            <input type="text" name="email" value="{{old('email', $user->email) }}"  required />
-        </label>
-        <br>
-        <label>
-            password:
-            <input type="text" name="password" value="{{old('password', $user->password) }}"  required />
-        </label>
-        <br>
-        <label>
-            role_id:
-            <input type="text" name="role_id" value="{{old('role_id', $user->role_id) }}"  required />
-        </label>
-        <br>
-        <label>
-            institution_id:
-            <input type="text" name="institution_id" value="{{old('institution_id', $user->institution_id) }}"  required />
-        </label>
-        <br>
-        </div>
-        <button type="submit"> update </button>
-    </form>
-</body>
-</html>
+<x-template-layout>
+
+    <div class="card mx-4 p-4">
+        <h5>Formulario de Edición de Usuarios</h5>
+        @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+        <form method="POST" action="{{ route('users.update', $user) }}">
+            @csrf
+            @method('PUT')
+            <label>Nombre:<input type="text" name="name" value="{{ old('name', $user->name) }}" required /></label>
+            <br>
+
+            <label>Apellido:<input type="text" name="email" value="{{ old('email', $user->email) }}" required /></label>
+            <br>
+
+            <label>Contraseña:<input type="text" name="password" value="{{ old('password', $user->password) }}" required /></label>
+            <br>
+
+            <label>role_id:<input type="text" name="role_id" value="{{ old('role_id', $user->role_id) }}" required /></label>
+            <br>
+
+            <label>institution_id:<input type="text" name="institution_id" value="{{ old('institution_id', $user->institution_id) }}"required /></label>
+            <br>
+
+            <button type="submit" class="btn btn-primary">Actualizar</button>
+            <a href="{{route('users.index')}}" class="btn btn-secondary">Volver</a>
+        </form>
+    </div>
+</x-template-layout>
