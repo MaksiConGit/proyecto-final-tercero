018dcbcf26c5b1249e89547b70975d246d0f51f3
EstefaniSaiquita <166553598+EstefaniSaiquita@users.noreply.github.com>
2024-11-19 23:39:51 -0300

    feat<front>: se crea componentes, views de profesores y alumnos

---
+++ code/resources/views/bocetos/alumnos.blade.php
@@ -0,0 +1,9 @@
+<x-template-layout>
+    <div class="container">
+        
+        <x-list-group-numered>
+            <x-slot name="titulo">Alumnos</x-slot> 
+            <x-slot name="nombre">tefi</x-slot>
+        </x-list-group-numered>
+    </div>
+</x-template-layout>
