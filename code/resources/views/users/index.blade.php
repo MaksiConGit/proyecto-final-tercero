3ff1adb58331e4dd3cf20bc4838b45a4302d7b76
Marzopax <lamarzopadelmar@gmail.com>
2024-11-20 19:19:15 -0300

    feat<front>: arreglo de template
    el index esta, la view intermedia que tiene el editar y eliminar no y el editar esta a la mitad

--- code/resources/views/users/index.blade.php
+++ code/resources/views/users/index.blade.php
@@ -1,25 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Lista de Usuarios</h1>
-    <a href="{{route('users.create')}}">create</a>
-    <ul>
-        @foreach ($users as $user)
-            <li><a href="{{route('users.show', [$user])}}"> {{$user->name}} @if ($user->is_deleted)(borrado) @endif </a></li>
-        @endforeach
-    </ul>
-    <hr>
+<x-template-layout>
+    
+    <div class="d-block m-auto bg-white p-5 pt-3 rounded border border-secondary border-opacity-100">
+        <h2 class="mb-4">Lista de Usuarios</h2>
+        {{-- <a href="{{ route('users.create') }}">create</a> --}}
+        <ul class="list-group">
+            @foreach ($users as $user)
+                <li class="list-group-item d-flex justify-content-center text-dark fw-semibold"><a href="{{ route('users.show', [$user]) }}"> {{ $user->name }} @if ($user->is_deleted)
+                            (borrado)
+                        @endif </a></li>
+            @endforeach
+        </ul>
+    </div>
+
+    <x-floating-icon-users></x-floating-icon-users>
+
+    {{-- <hr>
     <h4>Usuarios Eliminadas</h4>
     <ul>
         @foreach ($trashed as $trash)
             <li>{{$trash->name}}</li>
         @endforeach
-    </ul>
-</body>
-</html>
+    </ul> --}}
+
+</x-template-layout>
