cbf6d2b9f2059c4a8aeb998f4ec2bf73d7c7e577
Joako343 <joaquinborras0343@gmail.com>
2024-11-21 09:13:41 -0300

    fix<front>: reacion exams view
    ver los examenes como lista y boton de creacion

---
+++ code/resources/views/components/floating-icon-exams.blade.php
@@ -0,0 +1,7 @@
+<div class="buy-now">
+    <a        
+        href="{{route('exams.create')}}"
+        class="btn btn-primary btn-buy-now"
+        >+</a
+    >
+</div>
--- code/resources/views/exams/index.blade.php
+++ code/resources/views/exams/index.blade.php
@@ -1,42 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-template-layout>
+    <div class="d-block m-auto mt-0">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+        <h1>Lista de exámenes</h1>
+        <ul>
+            @foreach ($exams as $exam)
+                <li class="list-group list-group-flush">
+                    <a class="list-group-item list-group-item-action" href="{{ route('exams.show', [$exam]) }}">
+                        {{ $exam->teacherSubject->subject->name . ' ' . $exam->number }}</a>
+                    {{ $exam->teacherSubject->teacher ? '' : '(profesor no asignado)' }}
+                </li>
+            @endforeach
+        </ul>
+        {{-- <hr> --}}
+        {{-- <h4>Exámenes Eliminados</h4> --}}
+        {{-- <ul> --}}
+        {{-- @foreach ($trashed as $trash) --}}
 
-<body>
-    <h1>Lista de exámenes</h1>
-    <a href="{{ route('exams.create') }}">create</a>
-    <ul>
-        @foreach ($exams as $exam)
-            <li>
-                <a href="{{ route('exams.show', [$exam]) }}"> {{ $exam->teacherSubject->subject->name . " " . $exam->number }}</a>
-                {{$exam->teacherSubject->teacher ? '' : '(profesor no asignado)'}}
-            </li>
-        @endforeach
-    </ul>
-    <hr>
-    <h4>Exámenes Eliminados</h4>
-    <ul>
-        @foreach ($trashed as $trash)
-        
         {{-- Obtiene la materia del examen aunque estuviera eliminada  --}}
-        @php
+        {{-- @php
             $subject = $trash->teacherSubject->subject()->withTrashed()->first();
-        @endphp
-    
+        @endphp --}}
+
         {{-- Si la materia está eliminada, lo escribe igual y especifica que así es --}}
-        <li>
+        {{-- <li>
             {{ $subject->name . ' ' . $trash->number }}
             {{ $subject && $subject->trashed() ? '(materia eliminada)' : '' }}
         </li>
 
         @endforeach
-    </ul>
-</body>
-
-</html>
+    </ul> --}}
+    </div>
+    <x-floating-icon-exams></x-floating-icon-exams>
+</x-template-layout>
