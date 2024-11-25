b479737a5937e22b39366fc873fafccc9010cd06
Joako343 <joaquinborras0343@gmail.com>
2024-11-25 16:23:04 -0300

    fix<front>: Cursos en tablas
    Se muestran los cursos en forma de tablas

--- code/resources/views/courses/index.blade.php
+++ code/resources/views/courses/index.blade.php
@@ -1,5 +1,46 @@
 <x-template-layout>
     <div class="container">
+        <!-- Tabla con lista de Cursos -->
+        <x-table>
+
+            <x-slot name="titulo_tabla">Lista de Cursos</x-slot>
+
+            <x-slot name="table_head">
+                <thead>
+                    <tr>
+                        <th>Cursos</th>
+                        <th>Canridad de Alumnos</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+            </x-slot>
+            <x-slot name="table_body">
+                <tbody class="table-borde-bottom-0">
+                    @foreach ($courses as $course)
+                        <tr class="table-hover-row">
+                            <!-- Columna de Cursos -->
+                            <td>
+                                <a> 4A</a>
+                            </td>
+                            <!-- Cantidad de Alumnos -->
+                            <td>
+                                <a> 20</a>
+                            </td>
+                            <td>
+                                <!-- Aca iria las opciones eliminar y editar -->
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </x-slot>
+        </x-table>
+
+        <x-floating-icon-courses></x-floating-icon-courses>
+    </x-template-layout>
+
+
+{{-- <x-template-layout>
+    <div class="container">
         <h1>Cursos</h1>
         <a href="{{ route('courses.create') }}">create</a>
         <ul>
@@ -18,4 +59,4 @@
     </div>
 
     <x-floating-icon-courses></x-floating-icon-courses>
-</x-template-layout>
+</x-template-layout> --}}
