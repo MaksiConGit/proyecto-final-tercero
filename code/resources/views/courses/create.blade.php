a965716ce265d781e95910eb3a2a007796ed4ec4
Joako343 <joaquinborras0343@gmail.com>
2024-11-25 16:40:06 -0300

    fix<front>: create
    Se puede crear cursos

--- code/resources/views/courses/create.blade.php
+++ code/resources/views/courses/create.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -41,4 +41,48 @@
         <button type="submit"> create </button>
     </form>
 </body>
-</html>
+</html> --}}
+
+<x-template-layout>
+
+    <div class="card mx-4 p-4">
+        <h5>Formulario de Creacion de Cursos</h5>
+        @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+        <form method="POST" action="{{route('courses.store')}}">
+            @csrf
+            @method('PUT')
+            <label>
+                Nombre
+                <input type="text" name="name" value="{{old('name') }}"  required />
+            </label>
+            <br>
+            <br>
+            <label>
+                seccion:
+                <input type="text" name="section" value="{{old('section') }}"  required />
+            </label>
+            <br>
+            <br>
+            <label>
+                Carrera:
+                <select id="career_id" name="career_id" required>
+                    <option value="">Selecciona una Carrera</option>
+                    @foreach($careers as $career)
+                        <option value="{{ $career->id }}">{{ $career->name }}</option>
+                    @endforeach
+                </select>
+            </label>
+            <br>
+            <br>
+            <button type="submit" class="btn btn-primary">Crear</button>
+            <a href="{{route('courses.index')}}" class="btn btn-secondary">Volver</a>
+        </form>
+    </div>
+
+</x-template-layout>
