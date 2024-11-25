0e07b1eea77830496eb66622ee46f976fb1a3d78
Joako343 <joaquinborras0343@gmail.com>
2024-11-25 15:59:19 -0300

    fix<front>: Colocamos el template

--- code/resources/views/courses/index.blade.php
+++ code/resources/views/courses/index.blade.php
@@ -1,25 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Cursos</h1>
-    <a href="{{route('courses.create')}}">create</a>
-    <ul>
-        @foreach ($courses as $course)
-        <li><a href="{{route('courses.show', [$course])}}"> {{$course->course_number}}° {{$course->section}}</a></li>
-        @endforeach
-    </ul>
-    <hr>
-    <h4>Cursos Eliminadas</h4>
-    <ul>
-        @foreach ($trashed as $trash)
-            <li>{{$trash->course_number . "° " . $trash->section}}</li>
-        @endforeach
-    </ul>
-</body>
-</html>
+<x-template-layout>
+    <div class="container">
+        <h1>Cursos</h1>
+        <a href="{{ route('courses.create') }}">create</a>
+        <ul>
+            @foreach ($courses as $course)
+                <li><a href="{{ route('courses.show', [$course]) }}"> {{ $course->course_number }}°
+                        {{ $course->section }}</a></li>
+            @endforeach
+        </ul>
+        <hr>
+        <h4>Cursos Eliminadas</h4>
+        <ul>
+            @foreach ($trashed as $trash)
+                <li>{{ $trash->course_number . '° ' . $trash->section }}</li>
+            @endforeach
+        </ul>
+    </div>
+
+    <x-floating-icon></x-floating-icon>
+</x-template-layout>
