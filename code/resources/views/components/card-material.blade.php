ec62024872a69a9aa5a197504d48e77cd2f69bc1
MaksiConGit <122688647+MaksiConGit@users.noreply.github.com>
2024-11-26 14:57:28 -0300

    Revert "feat<front>: template materia profesor"
    This reverts commit fd292a30e17b75bd1f1d1d100eedd136c0dd3b8e.

--- code/resources/views/components/card-material.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="card p-2 mt-2">
-    <div class="card-header d-flex justify-content-center border-bottom border-secondary pb-2 pt-2">Material</div>
-    <div class="card-body">
-        <h5 class="card-title pt-3">Redes</h5>
-        <p class="card-text">Realizar el siguiente TP</p>
-        <a href="" class="btn btn-primary">Descargar Material</a>
-    </div>
-</div>
