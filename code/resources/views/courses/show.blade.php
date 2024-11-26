1952e1f7d5860583194c3a7b98da1d07d8132748
Joako343 <joaquinborras0343@gmail.com>
2024-11-26 13:12:03 -0300

    fix<front>: Show de cursos
    Se muestra el curso seleccionado, falta arreglar el botón de edición y que muestren los alumnos de ese curso

--- code/resources/views/courses/show.blade.php
+++ code/resources/views/courses/show.blade.php
@@ -1,20 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<x-template-layout>
+    <div class="card mx-4 p-4">
     <h1>Curso: {{$course->course_number . "° " . $course->section}}</h1>
     <p>Carrera "{{$course->career->name}}"</p>
-    <a href="{{route('courses.edit', $course)}}">Edit</a>
+    {{-- <a href="{{route('courses.edit', $course)}}">Edit</a> --}}
 
     <form method="POST" action="{{route('courses.destroy', $course)}}">
         @csrf
         @method('DELETE')
-        <button type="submit">Delete</button>
+        <button type="submit" class="btn btn-primary">Delete</button>
+        <button type="submit" class="btn btn-primary">Editar</button>
+        <a href="{{route('courses.index')}}" class="btn btn-secondary">Volver</a>
+        <br>
+        <br>
+        <p>Alumnos: <a href="">Joaquin, Lucas, David, Maximiliano, Estefi</a></p>
     </form>
-</body>
-</html>
+</div>
+</x-template-layout>
