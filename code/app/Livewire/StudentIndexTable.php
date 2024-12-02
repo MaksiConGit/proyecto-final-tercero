29cc2c39a9666fa651da6137106ab043de0d2d93
Lucas Del Labo <121238683+LucasDLabo@users.noreply.github.com>
2024-12-02 06:06:18 -0300

    feat: Filtro de student por institution, career y course
     - Se implementó un filtro a tiempo real para listar alumnos entre institución, carreras y cursos

---
+++ code/app/Livewire/StudentIndexTable.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Career;
+use App\Models\Course;
+use App\Models\Institution;
+use App\Models\Student;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class StudentIndexTable extends Component
+{
+    use WithPagination;
+
+    public $selectedInstitution = null;
+    public $selectedCareer = null;
+    public $selectedCourse = null;
+
+    public function render()
+    {
+        // Obtiene todas las instituciones
+        $institutions = Institution::all();
+        // Obtiene todas las carreras
+        $careers = $this->selectedInstitution ? Career::where('institution_id', $this->selectedInstitution)->get() : collect();
+
+        // Obtiene los cursos relacionados con la carrera seleccionada
+        $courses = $this->selectedCareer ? Course::where('career_id', $this->selectedCareer)->get() : collect();
+
+        // Filtra los estudiantes según la carrera y curso seleccionados
+        $students = Student::query()
+            ->when($this->selectedInstitution, function ($query) {
+                $query->whereHas('courses', function ($q) {
+                    $q->whereHas('career', function ($q) {
+                        $q->where('institution_id', $this->selectedInstitution);
+                    });
+                });
+            })
+            ->when($this->selectedCareer, function ($query) {
+                $query->whereHas('courses', function ($q) {
+                    $q->where('courses.career_id', $this->selectedCareer); // Especifica la tabla de carrera
+                });
+            })
+            ->when($this->selectedCourse, function ($query) {
+                $query->whereHas('courses', function ($q) {
+                    $q->where('courses.id', $this->selectedCourse); // Especifica la tabla de cursos
+                });
+            })
+            ->with('courses')
+            ->orderBy('id')
+            ->paginate(10);
+
+        return view('livewire.student-index-table', compact('institutions', 'careers', 'courses', 'students'));
+    }
+}
