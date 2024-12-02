3a72400a1e2b910192287aa544bdf7e38444d347
Joako343 <joaquinborras0343@gmail.com>
2024-12-02 15:59:23 -0300

    fix: carga datos reales 2
    Se crearon Users, Students & Teacher

--- code/database/seeders/PrincipalTeacherStudentSeeder.php
+++ code/database/seeders/PrincipalTeacherStudentSeeder.php
@@ -16,7 +16,7 @@ class PrincipalTeacherStudentSeeder extends Seeder
      */
     public function run(): void
     {
-        Principal::create([
+        Principal::create([ //PRINCIPAL
             'name' => 'No deberias ver esto',
             'lastname' => '',
             'dni' => '',
@@ -26,7 +26,7 @@ class PrincipalTeacherStudentSeeder extends Seeder
             'user_id' => '1',
             'deleted_at' => Carbon::now(),
         ]);
-        Principal::create([
+        Principal::create([ //PRINCIPAL
             'name' => 'Nicolás',
             'lastname' => 'Rotili',
             'dni' => '11111111',
@@ -36,7 +36,7 @@ class PrincipalTeacherStudentSeeder extends Seeder
             'user_id' => '2',
         ]);
 
-        Teacher::create([
+        Teacher::create([ //TEACHER
             'name' => 'No deberias ver esto',
             'lastname' => '',
             'dni' => '',
@@ -46,7 +46,7 @@ class PrincipalTeacherStudentSeeder extends Seeder
             'user_id' => '1',
             'deleted_at' => Carbon::now(),
         ]);
-        Teacher::create([
+        Teacher::create([ //TEACHER
             'name' => 'Karina',
             'lastname' => 'Gigli',
             'dni' => '11111111',
@@ -56,7 +56,7 @@ class PrincipalTeacherStudentSeeder extends Seeder
             'user_id' => '3',
         ]);
 
-        Student::create([
+        Student::create([ //STUDENT
             'name' => 'No deberias ver esto',
             'lastname' => '',
             'dni' => '',
@@ -66,7 +66,7 @@ class PrincipalTeacherStudentSeeder extends Seeder
             'user_id' => '1',
             'deleted_at' => Carbon::now(),
         ]);
-        Student::create([
+        Student::create([ //STUDENT
             'name' => 'Lucas',
             'lastname' => 'Del Labo',
             'dni' => '44867176',
@@ -75,7 +75,7 @@ class PrincipalTeacherStudentSeeder extends Seeder
             'city_id' => '2',
             'user_id' => '4',
         ]);
-        Student::create([
+        Student::create([ //STUDENT
             'name' => 'Joaquin',
             'lastname' => 'Borras',
             'dni' => '44867203',
@@ -83,6 +83,141 @@ class PrincipalTeacherStudentSeeder extends Seeder
             'birthdate' => '2003-06-27',
             'city_id' => '1',
             'user_id' => '5',
+        ]);
+        Student::create([ //STUDENT
+            'name' => 'David Ezquiel',
+            'lastname' => 'Carletta',
+            'dni' => '44241108',
+            'phone' => '543364695092',
+            'birthdate' => '2002-11-29',
+            'city_id' => '1',
+            'user_id' => '6',
+        ]);
+        Student::create([ //STUDENT
+            'name' => 'Estefani Naomí',
+            'lastname' => 'Saiquita',
+            'dni' => '44989001',
+            'phone' => '543364544144',
+            'birthdate' => '2003-06-27',
+            'city_id' => '1',
+            'user_id' => '7',
+        ]);
+        Student::create([ //STUDENT
+            'name' => 'Pedro',
+            'lastname' => 'Alberto',
+            'dni' => '42864563',
+            'phone' => '543364456654',
+            'birthdate' => '2001-03-22',
+            'city_id' => '1',
+            'user_id' => '8',
+        ]);
+        Student::create([ //STUDENT
+            'name' => 'Juan',
+            'lastname' => 'Ceta',
+            'dni' => '44567890',
+            'phone' => '543364987654',
+            'birthdate' => '2003-01-01',
+            'city_id' => '1',
+            'user_id' => '9',
+        ]);
+        Student::create([ //STUDENT
+            'name' => 'Luciano',
+            'lastname' => 'Bernadoti',
+            'dni' => '43224678',
+            'phone' => '543364235386',
+            'birthdate' => '2001-03-03',
+            'city_id' => '1',
+            'user_id' => '10',
+        ]);
+        Student::create([ //STUDENT
+            'name' => 'Maximiliano Ariel',
+            'lastname' => 'Alcaraz',
+            'dni' => '44568003',
+            'phone' => '54336427898',
+            'birthdate' => '2003-07-17',
+            'city_id' => '1',
+            'user_id' => '11',
+        ]);
+        Student::create([ //STUDENT
+            'name' => 'Lucardo',
+            'lastname' => 'No del Labo',
+            'dni' => '44543213',
+            'phone' => '54336563456',
+            'birthdate' => '2003-11-22',
+            'city_id' => '1',
+            'user_id' => '12',
+        ]);
+        Teacher::create([ //TEACHER
+            'name' => 'Nicolas',
+            'lastname' => 'Rotili',
+            'dni' => '36987234',
+            'phone' => '3364987123',
+            'birthdate' => Carbon::yesterday(),
+            'city_id' => '2',
+            'user_id' => '12',
+        ]);
+        Teacher::create([ //TEACHER
+            'name' => 'Walter',
+            'lastname' => 'Bur',
+            'dni' => '32846571',
+            'phone' => '3364987102',
+            'birthdate' => Carbon::yesterday(),
+            'city_id' => '2',
+            'user_id' => '14',
+        ]);
+        Teacher::create([ //TEACHER
+            'name' => 'Karina',
+            'lastname' => 'Gigli',
+            'dni' => '32194567',
+            'phone' => '3364555999',
+            'birthdate' => Carbon::yesterday(),
+            'city_id' => '2',
+            'user_id' => '15',
+        ]);
+        Teacher::create([ //TEACHER
+            'name' => 'Nancy',
+            'lastname' => 'Cabral',
+            'dni' => '33876145',
+            'phone' => '3364008765',
+            'birthdate' => Carbon::yesterday(),
+            'city_id' => '2',
+            'user_id' => '16',
+        ]);
+        Teacher::create([ //TEACHER
+            'name' => 'Luis',
+            'lastname' => 'De Ambrosio',
+            'dni' => '35750087',
+            'phone' => '3364778899',
+            'birthdate' => Carbon::yesterday(),
+            'city_id' => '2',
+            'user_id' => '17',
+        ]);
+        Teacher::create([ //TEACHER
+            'name' => 'Facundo',
+            'lastname' => 'Perazzo',
+            'dni' => '33456009',
+            'phone' => '3364987654',
+            'birthdate' => Carbon::yesterday(),
+            'city_id' => '2',
+            'user_id' => '18',
+        ]);
+        Teacher::create([ //TEACHER
+            'name' => 'Fabian',
+            'lastname' => 'Turino',
+            'dni' => '30876909',
+            'phone' => '3364098172',
+            'birthdate' => Carbon::yesterday(),
+            'city_id' => '2',
+            'user_id' => '19',
+        ]);
+        Teacher::create([ //TEACHER
+            'name' => 'Damian',
+            'lastname' => 'Moltoni',
+            'dni' => '34590078',
+            'phone' => '3364107385',
+            'birthdate' => Carbon::yesterday(),
+            'city_id' => '2',
+            'user_id' => '20',
         ]);
         Principal::factory(10)->create();
         Teacher::factory(10)->create();
--- code/database/seeders/UserSeeder.php
+++ code/database/seeders/UserSeeder.php
@@ -63,7 +63,141 @@ class UserSeeder extends Seeder
             'accountable_type' => 'App\Models\Student',
             'accountable_id' => '3'
         ])->assignRole('Student');
-
+        User::create([
+            'name' => 'Marzopax', //ESTUDIANTE
+            'email' => 'lamarzopadelmar@gmail.com',
+            'email_verified_at' => Carbon::yesterday(),
+            'password' => '1234',
+            'institution_id' => '3',
+            'accountable_type' => 'App\Models\Student',
+            'accountable_id' => '4'
+        ])->assignRole('Student');
+        User::create([
+            'name' => 'Tefi', //ESTUDIANTE
+            'email' => 'estefimail@gmail.com',
+            'email_verified_at' => Carbon::yesterday(),
+            'password' => '1234',
+            'institution_id' => '3',
+            'accountable_type' => 'App\Models\Student',
+            'accountable_id' => '5'
+        ])->assignRole('Student');
+        User::create([
+            'name' => 'Pedro', //ESTUDIANTE
+            'email' => 'Pedro@gmail.com',
+            'email_verified_at' => Carbon::yesterday(),
+            'password' => '1234',
+            'institution_id' => '3',
+            'accountable_type' => 'App\Models\Student',
+            'accountable_id' => '6'
+        ])->assignRole('Student');
+        User::create([
+            'name' => 'Juanceta', //ESTUDIANTE
+            'email' => 'Juanceta@gmail.com',
+            'email_verified_at' => Carbon::yesterday(),
+            'password' => '1234',
+            'institution_id' => '3',
+            'accountable_type' => 'App\Models\Student',
+            'accountable_id' => '7'
+        ])->assignRole('Student');
+        User::create([
+            'name' => 'Luciano', //ESTUDIANTE
+            'email' => 'BiggusDickus@gmail.com',
+            'email_verified_at' => Carbon::yesterday(),
+            'password' => '1234',
+            'institution_id' => '3',
+            'accountable_type' => 'App\Models\Student',
+            'accountable_id' => '8'
+        ])->assignRole('Student');
+        User::create([
+            'name' => 'Maksi', //ESTUDIANTE
+            'email' => 'Maximiliano@gmail.com',
+            'email_verified_at' => Carbon::yesterday(),
+            'password' => '1234',
+            'institution_id' => '3',
+            'accountable_type' => 'App\Models\Student',
+            'accountable_id' => '9'
+        ])->assignRole('Student');
+        User::create([
+            'name' => 'Lukita', //ESTUDIANTE
+            'email' => 'LucasdelLabo@gmail.com',
+            'email_verified_at' => Carbon::yesterday(),
+            'password' => '1234',
+            'institution_id' => '3',
+            'accountable_type' => 'App\Models\Student',
+            'accountable_id' => '10'
+        ])->assignRole('Student');
+        User::create([
+            'name' => 'Nico', //TEACHER
+            'email' => 'nicorotili@gmail.com',
+            'email_verified_at' => Carbon::yesterday(),
+            'password' => '1234',
+            'institution_id' => '2',
+            'accountable_type' => 'App\Models\Teacher',
+            'accountable_id' => '3',
+        ])->assignRole('Teacher');
+        User::create([
+            'name' => 'Walter Bur', //TEACHER
+            'email' => 'BurWalter@gmail.com',
+            'email_verified_at' => Carbon::yesterday(),
+            'password' => '1234',
+            'institution_id' => '2',
+            'accountable_type' => 'App\Models\Teacher',
+            'accountable_id' => '4',
+        ])->assignRole('Teacher');
+        User::create([
+            'name' => 'Karina', //TEACHER
+            'email' => 'Karina@gmail.com',
+            'email_verified_at' => Carbon::yesterday(),
+            'password' => '1234',
+            'institution_id' => '2',
+            'accountable_type' => 'App\Models\Teacher',
+            'accountable_id' => '5',
+        ])->assignRole('Teacher');
+        User::create([
+            'name' => 'Nancy', //TEACHER
+            'email' => 'NancyCabral@gmail.com',
+            'email_verified_at' => Carbon::yesterday(),
+            'password' => '1234',
+            'institution_id' => '2',
+            'accountable_type' => 'App\Models\Teacher',
+            'accountable_id' => '6',
+        ])->assignRole('Teacher');
+        User::create([
+            'name' => 'Luis', //TEACHER
+            'email' => 'LuuisdeAmbrosio@gmail.com',
+            'email_verified_at' => Carbon::yesterday(),
+            'password' => '1234',
+            'institution_id' => '2',
+            'accountable_type' => 'App\Models\Teacher',
+            'accountable_id' => '7',
+        ])->assignRole('Teacher');
+        User::create([
+            'name' => 'Facu', //TEACHER
+            'email' => 'FacundoPerazzo@gmail.com',
+            'email_verified_at' => Carbon::yesterday(),
+            'password' => '1234',
+            'institution_id' => '2',
+            'accountable_type' => 'App\Models\Teacher',
+            'accountable_id' => '8',
+        ])->assignRole('Teacher');
+        User::create([
+            'name' => 'Fabian', //TEACHER
+            'email' => 'FabianTurino@gmail.com',
+            'email_verified_at' => Carbon::yesterday(),
+            'password' => '1234',
+            'institution_id' => '2',
+            'accountable_type' => 'App\Models\Teacher',
+            'accountable_id' => '9',
+        ])->assignRole('Teacher');
+        User::create([
+            'name' => 'Damian', //TEACHER
+            'email' => 'DamianMoltoni@gmail.com',
+            'email_verified_at' => Carbon::yesterday(),
+            'password' => '1234',
+            'institution_id' => '2',
+            'accountable_type' => 'App\Models\Teacher',
+            'accountable_id' => '10',
+        ])->assignRole('Teacher');
         User::factory(70)->create();
     }
 }
